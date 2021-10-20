8eb6de8fab79a9e672cfcbc9d147be7e4dd1a850
brianqp <brianqp9889@gmail.com>
2021-10-19 23:25:11 -0500

    Avance para detectar rostros!

--- reconocimiento_facial/reconociendo_rostros.py
+++ reconocimiento_facial/reconociendo_rostros.py
@@ -7,10 +7,10 @@ gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
 faceClassif =cv2.CascadeClassifier(".//data//haarcascade//haarcascade_frontalface_default.xml")
 
-faces = faceClassif.detectMultiScale(gray, scaleFactor=1.1,
-                                     minNeighbors=5,
-                                     minSize=(30,30),
-                                     maxSize=(200,200))
+faces = faceClassif.detectMultiScale(gray, scaleFactor=1.1, # Variable que afecta que tanto se reducira la imagen
+                                     minNeighbors=5,    # Minima cantidad de vecinos que son la misma persona
+                                     minSize=(50,50),   # Tamano minimo de los rostros
+                                     maxSize=(200,200)) # Tamano maximo de los rostros
 
 for (x,y,w,h) in faces:
     cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0))
